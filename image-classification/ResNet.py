f961e2b3c08eb6b3d5859ee374674b173e064c03
Liu Pengxiang <pxliu_phd@seu.edu.cn>
2020-08-08 21:26:36 +0800

    update

--- image-classification/ResNet.py
+++ image-classification/ResNet.py
@@ -310,7 +310,40 @@ def image_classification(layers = '34', transfer = True):
 
 
 if __name__ == "__main__":
-    
+    '''
     layers = ['18', '34', '50']
     for layer in layers:
         image_classification(layers = layer, transfer = True)
+    '''
+    # predict
+    data_transform = transforms.Compose(
+        [transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    img = Image.open("./cat.jpg")
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim = 0)
+
+    # read class_indict
+    try:
+        json_file = open('./pascal_voc_classes.json', 'r')
+        class_indict = json.load(json_file)
+    except Exception as e:
+        print(e)
+        exit(-1)
+    
+    model = build_resnet('34', 20)
+    model_weight_path = 'D:\\Data Set\\transfer_path\\my_path\\resNet34-2020-08-07.pth'
+    model.load_state_dict(torch.load(model_weight_path))
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img))
+        predict = torch.softmax(output, dim = 0)
+        predict_cla = torch.argmax(predict).numpy()
+    print(class_indict[str(predict_cla)], predict[predict_cla].numpy())
+    plt.show()
